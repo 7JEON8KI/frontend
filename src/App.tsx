6a2ca34837f26dee7c94eb3da735321f36c241d2
y00n-lee <0613myday@gmail.com>
2024-02-13 06:59:26 +0900

    eslint 및 prettier 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="p-4 pt-2">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
 }
