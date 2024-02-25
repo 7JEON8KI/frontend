ebf77e4335dba0fa9e81c22bd019767da7e390b1
y00n-lee <0613myday@gmail.com>
2024-02-25 23:31:54 +0900

    헤더 링크 연결

--- src/components/mealkeat/commons/Header.tsx
+++ src/components/mealkeat/commons/Header.tsx
@@ -6,8 +6,10 @@ import CartPath from "assets/images/icons/Cart.png";
 import UserPath from "assets/images/icons/User.png";
 import MenuPath from "assets/images/icons/Menu.png";
 import { StyledHeader, StyledSearch, StyledIconList, StyledIcon, StyledTopNav, NavMenu } from "./Header.style";
+import { useNavigate } from "react-router-dom";
 
 export const Header = (): JSX.Element => {
+  const navigate = useNavigate();
   return (
     <StyledHeader>
       <div className="frame">
@@ -19,32 +21,42 @@ export const Header = (): JSX.Element => {
           </label>
         </StyledSearch>
         <StyledIconList>
-          <StyledIcon>
+          <StyledIcon onClick={() => navigate("/like")}>
             <img src={HeartPath} alt="찜 아이콘" />
             <span>찜 목록</span>
           </StyledIcon>
-          <StyledIcon>
+          <StyledIcon onClick={() => navigate("/cart")}>
             <img src={CartPath} alt="장바구니 아이콘" />
             <span>장바구니</span>
             {/* <StyledCnt>
                 <span>20</span>
               </StyledCnt> */}
           </StyledIcon>
-          <StyledIcon>
+          <StyledIcon onClick={() => navigate("/mypage")}>
             <img src={UserPath} alt="마이페이지 아이콘" />
             <span>마이페이지</span>
           </StyledIcon>
         </StyledIconList>
         <StyledTopNav>
-          <NavMenu title="클릭 시 전체상품 페이지로 이동">
+          <NavMenu title="클릭 시 전체상품 페이지로 이동" onClick={() => navigate("/list")}>
             <img src={MenuPath} alt="메뉴 아이콘" />
             전체상품
           </NavMenu>
-          <NavMenu title="클릭 시 밀킷 추천 페이지로 이동">밀킷 추천</NavMenu>
-          <NavMenu title="클릭 시 오븐 페이지로 이동">오븐</NavMenu>
-          <NavMenu title="클릭 시 이벤트 페이지로 이동">이벤트</NavMenu>
-          <NavMenu title="클릭 시 베스트 페이지로 이동">베스트</NavMenu>
-          <NavMenu title="클릭 시 할인 중 페이지로 이동">할인 중</NavMenu>
+          <NavMenu title="클릭 시 밀킷 추천 페이지로 이동" onClick={() => navigate("/recommand")}>
+            밀킷 추천
+          </NavMenu>
+          <NavMenu title="클릭 시 오븐 페이지로 이동" onClick={() => navigate("/oven")}>
+            오븐
+          </NavMenu>
+          <NavMenu title="클릭 시 이벤트 페이지로 이동" onClick={() => navigate("/event")}>
+            이벤트
+          </NavMenu>
+          <NavMenu title="클릭 시 베스트 페이지로 이동" onClick={() => navigate("/best")}>
+            베스트
+          </NavMenu>
+          <NavMenu title="클릭 시 할인 중 페이지로 이동" onClick={() => navigate("/discount")}>
+            할인 중
+          </NavMenu>
         </StyledTopNav>
       </div>
     </StyledHeader>
--- src/components/mealkeat/commons/Logo.tsx
+++ src/components/mealkeat/commons/Logo.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import logo from "assets/images/logo.png";
 import { StyledLogo } from "./Logo.style";
+import { useNavigate } from "react-router-dom";
 
 const Logo = (): JSX.Element => {
+  const navigate = useNavigate();
   return (
-    <StyledLogo>
+    <StyledLogo onClick={() => navigate("/")}>
       <img src={logo} alt="mealkeat 로고" title="클릭 시 메인으로 이동" />
     </StyledLogo>
   );
