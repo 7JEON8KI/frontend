83eda8425bf692623457a1d92782d37fa605fcd3
y00n-lee <0613myday@gmail.com>
2024-03-06 11:49:10 +0900

    이미지 검색 페이지 수정, 밀킷 결과 NoProduct 추가

--- src/pages/mealkeat/product/PageSearchResult.tsx
+++ src/pages/mealkeat/product/PageSearchResult.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Layout, Product } from "components/mealkeat";
+import { Layout, Product, NoProduct } from "components/mealkeat";
 import {
   StyledListGrid,
   StyledSidebarDiv,
@@ -18,10 +18,12 @@ import scrollToTop from "utils/scrollToTop";
 import productApi from "apis/productApi";
 import { useLocation } from "react-router-dom";
 import { ProductResponseDTO } from "models/mealkeat/ProductModels";
+import { ProductRecommendResponse } from "models/mealkeat/RecommendModels";
 
 const PageSearchResult: React.FC = () => {
   const location = useLocation();
   const searchWord = location?.state?.searchWord || "";
+  const imageProduct = location?.state?.imageProduct || [];
   const [productList, setProductList] = React.useState<ProductResponseDTO[]>([]);
 
   const initRequest = {
@@ -30,13 +32,40 @@ const PageSearchResult: React.FC = () => {
     },
   };
 
+  const createProductObject = async (object: ProductRecommendResponse[]): Promise<ProductResponseDTO[]> => {
+    return object?.map(
+      obj =>
+        ({
+          amount: 1,
+          calorie: 0,
+          createdAt: [],
+          discountRate: obj.discountRate,
+          isLike: 0,
+          modifiedAt: [],
+          price: obj.price,
+          productDetail: "",
+          productId: obj.productId,
+          productName: obj.productName,
+          productSubName: "",
+          productType: obj.productType,
+          rn: 0,
+          stock: 1,
+          storage: "",
+          themeName: null,
+          thumbnailImageUrl: obj.mainImgUrl,
+        }) as unknown,
+    ) as ProductResponseDTO[];
+  };
+
   const getSearchProduct = async () => {
     const fetchData = await productApi.getProductsWithSearch({ ...initRequest });
-    setProductList(fetchData.data);
+    setProductList(fetchData.data as ProductResponseDTO[]);
   };
 
   useEffect(() => {
-    getSearchProduct();
+    if (imageProduct.length > 0) {
+      createProductObject(imageProduct).then(productObj => setProductList(productObj));
+    } else getSearchProduct();
     return () => {};
   }, [location?.key]);
 
@@ -46,14 +75,20 @@ const PageSearchResult: React.FC = () => {
         <StyledSidebarDiv />
         <StyledMain>
           <StyledMenuNav>
-            <StyledMenuTitle>{`'${searchWord}'에 대한 검색 결과입니다`}</StyledMenuTitle>
+            <StyledMenuTitle>
+              {imageProduct.length > 0 ? "이미지로 검색한 결과입니다" : `'${searchWord}'에 대한 검색 결과입니다`}
+            </StyledMenuTitle>
             <StyledMenuInfo>
               <StyledItemCount>{`총 ${productList?.length || 0}건`}</StyledItemCount>
             </StyledMenuInfo>
           </StyledMenuNav>
-          <StyledProductGrid>
-            {productList?.map(product => <Product key={product.productId} product={product} />)}
-          </StyledProductGrid>
+          {productList.length > 0 ? (
+            <StyledProductGrid>
+              {productList?.map(product => <Product key={product.productId} product={product} />)}
+            </StyledProductGrid>
+          ) : (
+            <NoProduct />
+          )}
         </StyledMain>
         <StyledSidebarDiv>
           <StyledSidebarAside>
