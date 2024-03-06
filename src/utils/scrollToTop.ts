083d2afc3b1797ea2ed3a5744113575d748d0ff8
y00n-lee <0613myday@gmail.com>
2024-03-06 11:47:43 +0900

    scrollToTop behavior 초기값 설정

--- src/utils/scrollToTop.ts
+++ src/utils/scrollToTop.ts
@@ -2,7 +2,7 @@ interface ScrollToTopProps {
   smooth?: boolean;
 }
 
-const scrollToTop = ({ smooth }: ScrollToTopProps) => {
+const scrollToTop = ({ smooth = false }: ScrollToTopProps) => {
   window.scrollTo({ top: 0, behavior: smooth ? "smooth" : "auto" });
 };
 
